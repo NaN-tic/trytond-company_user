7d768cfc39a9dff6e57e56d73d8760512e9f3253
Carlos G?lvez <carlos.galvez@nan-tic.com>
2019-03-19 11:40:11 +0100

    Increase version. Upgrade to python 3.

--- company.py
+++ company.py
@@ -6,8 +6,7 @@ from trytond.pool import PoolMeta
 __all__ = ['Company']
 
 
-class Company:
-    __metaclass__ = PoolMeta
+class Company(metaclass=PoolMeta):
     __name__ = 'company.company'
     intercompany_user = fields.Many2One('res.user', 'Company User',
         help='User with company rules when create to other company.')
